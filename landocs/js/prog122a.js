cc6df690560b2fab8e6c6c7010bdfa9f1c8c5acf
Timothy Smith <smith.t7@stu.janesvill.k12.wi.us>
2025-04-22 14:20:49 -0500

    4/22

--- landocs/js/prog122a.js
+++ landocs/js/prog122a.js
@@ -1,15 +1,17 @@
 function doMath() {
     let out = "Number&#8195&#8195Square&#8195&#8195Square Root<br>";
-    for (let lcv = 1; lcv <= 50; lcv++) {
+    let lcv = 1;
+    while (lcv <= 50) {
         out += lcv;
         if (lcv < 10) {
             out += (lcv * lcv).toString().padStart(12, " ");
-            out += Math.sqrt(lcv).toFixed(3).toString().padStart(11, " ") + "<br>";
+            out += Math.sqrt(lcv).toFixed(4).toString().padStart(11, " ") + "<br>";
         }
         else {
             out += (lcv * lcv).toString().padStart(11, " ");
-            out += Math.sqrt(lcv).toFixed(3).toString().padStart(11, " ") + "<br>";
+            out += Math.sqrt(lcv).toFixed(4).toString().padStart(11, " ") + "<br>";
         }
+        lcv++;
     }
     document.getElementById("output").innerHTML = out;
 }
--- landocs/js/prog122b.js
+++ landocs/js/prog122b.js
@@ -1,6 +1,7 @@
 function doMath() {
     let out = "Hours&#8195&#8195&#8195&#8195&#8195Pay" + "<br>";
-    for (let lcv = 1; lcv <= 50; lcv++) {
+    let lcv = 1;
+    while (lcv <= 40) {
         out += lcv;
         if (lcv < 10) {
             out += (lcv * 4).toString().padStart(12, " ") + "<br>";
@@ -8,6 +9,7 @@ function doMath() {
         else {
             out += (lcv * lcv).toString().padStart(11, " ") + "<br>";
         }
+        lcv++;
     }
     document.getElementById("output").innerHTML = out;
 }
