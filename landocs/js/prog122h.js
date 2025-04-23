c63fd80368c44c05098aed4beedfe0c92a43b7e2
Timothy Smith <smith.t7@stu.janesvill.k12.wi.us>
2025-04-23 11:50:05 -0500

    4/23

--- landocs/js/prog122h.js
+++ landocs/js/prog122h.js
@@ -1,11 +1,1 @@
-function doMath() {
-    let out = "Number      Square      Square Root      Cube      4th Root\n";
-    for (let i = 1; i < 21; i++) {
-        out += i.toString().padStart(2, " ") +
-            (Math.pow(i,2)).toString().padStart(16, " ")  +
-            (Math.pow(i, (1/2))).toFixed(4).toString().padStart(16, " ")+
-            (Math.pow(i,3)).toString().padStart(11, " ")  +
-            (Math.pow(i, (1/4))).toFixed(4).toString().padStart(14, " ")+ "\n";
-    }
-    document.getElementById("output").innerHTML = out;
-}
+function doMath() { let out = "Number      Square      Square Root      Cube      4th Root\n"; for (let i = 1; i < 21; i++) { out += i.toString().padStart(2, " ") + (Math.pow(i,2)).toString().padStart(16, " ")  + (Math.pow(i, (1/2))).toFixed(4).toString().padStart(16, " ")+ (Math.pow(i,3)).toString().padStart(11, " ")  + (Math.pow(i, (1/4))).toFixed(4).toString().padStart(14, " ")+ "\n"; } document.getElementById("output").innerHTML = out; }
